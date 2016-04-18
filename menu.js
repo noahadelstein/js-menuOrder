cb96ec84bb328397c5ccd7c39d84bdcf2c66a3b5
noahadelstein <noadelstein@gmail.com>
2016-04-18 18:35:48 -0500

    a

--- menu.js
+++ menu.js
@@ -4,12 +4,29 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
-}
+function question(item, choice) {
+  var answerInput= prompt(item);
+     if ( answerInput.toUpperCase() === choice ) {
+     order += item + " and ";
+     }
+     else {
+       order += "no " + item + " and ";
+     }
 
+}
 // call the QUESTION function with the order options
-question();
+question("Chicken", "YES");
+question("Steak", "YES");
+question("Black Beans", "YES");
+question("Pinto Beans", "YES");
+question("White Rice", "YES");
+question("Brown Rice", "YES");
+question("Salsa", "YES");
+question("Guac", "YES");
+question("Drink", "YES");
+
+
+
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+document.write("<p>You ordered: " + order + "that is all<p>");
